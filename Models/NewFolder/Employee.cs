5f349f3157350b57ffde6051009b6fe394cec323
ShahzadAli1612 <2020cs613@student.uet.edu.pk>
2024-01-30 01:00:33 +0500

    Deployed Web App Uploaded

---
+++ Models/NewFolder/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace GymManagment.Models.NewFolder
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DOB { get; set; }
+        public string Department { get; set; }
+
+
+        
+    }
+}
---
+++ NewFolder/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GymManagment.Models.NewFolder;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagment.NewFolder
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } 
+    }
+}
